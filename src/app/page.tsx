d011d3b9b9e4345632085c11e2e7b630276ebd24
Ayush Singh <ayushsingh30082005@gmail.com>
2025-05-30 19:13:47 +0530

    Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      Home
+      <Button>Click me</Button>
     </div>
   );
 }
