44392fdf592a218e45a337a65251c597a48864a2
Ayush Singh <ayushsingh30082005@gmail.com>
2025-06-03 22:37:57 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,7 @@ import { SignedIn, SignedOut, UserProfile } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <Button variant="secondary">Sign In</Button>
-      </SignedOut>
-      <SignedIn>
-        <UserProfile />
-      </SignedIn>
+      Hello
     </div>
   );
 }
